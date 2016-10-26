5e023c83690e42fb1e5a1efe40169734c582ae2f
yuraermolenko <yura14ermolenko@gmail.com>
2016-10-26 15:46:31 +0300

    db

--- js/sessionStore.js
+++ js/sessionStore.js
@@ -4,11 +4,11 @@ const mySQLStore = require('express-mysql-session')(session);
 
 
 const sessionStore = new mySQLStore({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'qwerrewqasdffdsa',
-    database: 'data',
+    host: 'nodejs-8267.mysql.dbs.appsdeck.eu',
+    user: 'nodejs_8267',
+    password: 'GTm56QFAngJ3H36ZfsTi',
+    database: 'nodejs_8267',
+    port: 30895
     checkExpirationInterval: 900000,
     expiration: 86400000
 });
